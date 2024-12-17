f213f18a4b8f6bd75a3c1f38fe64554a7f91e24b
SantiGomez2519 <sgomezo14@eafit.edu.co>
2024-12-17 17:49:06 -0500

    Game working correctly

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ class Game:
         self.screen.set_screen()
         self.player.draw(self.screen.screen)
         self.pipe = pipe.Pipe()
+        self.lose = False
 
 
     def game_running(self):
@@ -28,9 +29,33 @@ class Game:
             # Draw the pipe
             self.pipe.draw(self.screen.screen)
             self.pipe.move()
+
+            # Lose if the player hits the pipe
+            if self.player.x + self.player.width > self.pipe.x and self.player.x < self.pipe.x + self.pipe.width:
+                if self.player.y < self.pipe.height or self.player.y + self.player.height > self.pipe.height + 200:
+                    self.lose = True
+
+            if self.lose:
+                # Freeze the screen by skipping updates
+                self.draw_lose_message()
+                pygame.display.flip()
+                self.screen.clock.tick(0)  # Limit FPS in lose state
+                if pygame.key.get_pressed()[pygame.K_SPACE]:
+                    self.lose = False
+                    self.player.y = 400
+                    self.pipe.x = 800
+                    self.game_running()
             
 
             # flip() the display to put your work on screen
             pygame.display.flip()
 
             self.screen.clock.tick(60)  # limits FPS to 60
+
+    def draw_lose_message(self):
+        # Draw a 'Paused' message on the screen.
+        font = pygame.font.Font(None, 74)
+        text = font.render("You failed", True, (255, 255, 255))
+        text_rect = text.get_rect(center=(self.screen.width // 2, self.screen.height // 2))
+        self.screen.screen.blit(self.screen.background, (0, 0))  # Keep the background visible
+        self.screen.screen.blit(text, text_rect)
--- pipe.py
+++ pipe.py
@@ -5,15 +5,18 @@ class Pipe:
     def __init__(self):
         self.x = 800
         self.y = 0
-        self.width = 100
-        self.height = random.randint(100, 400)
+        self.width = 80
+        self.height = random.randint(75, 350)
 
     def draw(self, screen):
         pipe = pygame.image.load("./Icons/pipe.png").convert_alpha()
         pipe = pygame.transform.scale(pipe, (self.width, self.height))
         pipe = pygame.transform.rotate(pipe, 180)
+
         pipe2  = pipe.copy()
-        pipe2 = pygame.transform.flip(pipe2, False, True)
+        pipe2 = pygame.transform.scale(pipe2, (self.width, 600 - self.height))
+        pipe2 = pygame.transform.rotate(pipe2, 180)
+
         screen.blit(pipe, (self.x, self.y))
         screen.blit(pipe2, (self.x, self.height + 200))
 
